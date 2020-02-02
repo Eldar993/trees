536c42fc3db0965a64047986fc888093a598f627
Eldar <eldar7989@gmail.com>
2020-02-02 21:38:01 +0100

    Add new methods contain,delete,size,heigth

--- src/main/java/com/company/Program.java
+++ src/main/java/com/company/Program.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Program {
@@ -17,17 +21,17 @@ public class Program {
             if (index == 0) {
                 insert();
             } else if (index == 1) {
-//                insertRandom();
+ //               insertRandom();
             } else if (index == 2) {
-//                delete();
+                delete();
             } else if (index == 3) {
-//                clear();
+               clear();
             } else if (index == 4) {
-//                contain();
+                contain();
             } else if (index == 5) {
-//                height();
+                height();
             } else if (index == 6) {
-//                size();
+                size();
             } else if (index == 7) {
 //                rbtree.Print();
                 scanner.nextLine();
@@ -51,7 +55,7 @@ public class Program {
 
     static void insert() {
 
-        System.out.println("key = ");
+        System.out.print("key = ");
         int i = scanner.nextInt();
         scanner.nextLine();
 
@@ -60,7 +64,7 @@ public class Program {
         } else {
             System.out.println("Key " + i + " was NOT added in tree");
         }
-//        if (rbtree.Add(i)) {
+//        if (rbtree.add(i)) {
 //            System.out.println("Key " + i + " was added in rb-tree");
 //        } else {
 //            System.out.println("Key " + i + " was NOT added in rb-tree");
@@ -70,95 +74,92 @@ public class Program {
     /*static void insertRandom() {
         int count;
         do {
-            count = ReadInt("Count = ");
+            System.out.println("Count = ");
+            count = scanner.nextInt();
         } while (count <= 0);
 
 
-        List<MenuItem> items = new List<MenuItem>();
-        items.Add(new MenuItem("random    ", '1'));
-        items.Add(new MenuItem("ascending ", '2'));
-        items.Add(new MenuItem("descending", '3'));
+        List<MenuItem> items = new ArrayList<>();
+        items.add(new MenuItem("random    ", "1"));
+        items.add(new MenuItem("ascending ", '2'));
+        items.add(new MenuItem("descending", '3'));
 
         Menu submenu = new Menu();
         submenu.setItems(items);
         int index;
         List<Integer> list = generateList(count, 100);
         do {
-            index = submenu.Choose();
+            index = submenu.choose();
             if (index == 0) {
                 break;
             } else if (index == 1) {
-                list.Sort();
+                list.sort();
                 break;
             } else if (index == 2) {
-                list.Sort((a, b) => -1 * a.CompareTo(b));
+                list.sort((a, b) => -1 * a.CompareTo(b));
                 break;
             }
         } while (true);
 
-        int addedInTree = Fill(list, tree);
-        int addedInRBTree = Fill(list, rbtree);
-        System.out.Clear();
+        int addedInTree = fill(list, tree);
+        //int addedInRBTree = Fill(list, rbtree);
         System.out.println("Try to add " + list.size() + " element(s): ");
-        System.out.println(String.Join(", ", list));
+        System.out.println(String.join(", ", list));
         System.out.println(addedInTree + " node(s) was added in binary search tree");
-        System.out.println(addedInRBTree + " node(s) was added  in red black tree");
-        System.out.ReadKey(false);
-    }
+        //System.out.println(addedInRBTree + " node(s) was added  in red black tree");
+    }*/
 
     static void delete() {
-        int i = ReadInt("Deleted node = ");
-        if (tree.Delete(i)) {
+        System.out.println("Deleted node = ");
+        int i = scanner.nextInt();
+        if (tree.delete(i)) {
             System.out.println("Key " + i + " was deleted in tree");
         } else {
             System.out.println("Key " + i + " was NOT deleted in tree");
         }
-        if (rbtree.Delete(i)) {
+       /* if (rbtree.Delete(i)) {
             System.out.println("Key " + i + " was deleted in rb-tree");
         } else {
             System.out.println("Key " + i + " was NOT deleted in rb-tree");
-        }
-        System.out.ReadKey(false);
+        }*/
     }
 
     static void clear() {
-        tree.Clear();
-        rbtree.Clear();
+        tree.clear();
+        //rbtree.clear();
         System.out.println("Binary search and red black trees were cleared");
-        System.out.ReadKey(false);
     }
 
     static void contain() {
-        int i = ReadInt("Contains node = ");
-        if (tree.Contains(i)) {
+        System.out.println("Contains node = ");
+        int i = scanner.nextInt();
+        if (tree.contains(i)) {
             System.out.println("Key " + i + " was found in tree");
         } else {
             System.out.println("Key " + i + " was NOT found in tree");
         }
-        if (rbtree.Contains(i)) {
+       /* if (rbtree.contains(i)) {
             System.out.println("Key " + i + " was found in rb-tree");
         } else {
             System.out.println("Key " + i + " was NOT found in rb-tree");
-        }
-        System.out.ReadKey(false);
+        }*/
     }
 
     static void size() {
-        System.out.println("Tree size:  " + tree.Size());
-        System.out.println("RB-Tree size:  " + rbtree.Size());
-        System.out.ReadKey(false);
+        System.out.println("Tree size:  " + tree.size());
+        //System.out.println("RB-Tree size:  " + rbtree.Size());
     }
 
     static void height() {
-        System.out.println("Tree height:  " + tree.Height());
-        System.out.println("RB-Tree height:  " + rbtree.Height());
-        System.out.println("RB-Tree black height:  " + ((RedBlackTree<Integer>)rbtree).BlackHeight());
+        System.out.println("Tree height:  " + tree.height());
+        //System.out.println("RB-Tree height:  " + rbtree.height());
+        //System.out.println("RB-Tree black height:  " + ((RedBlackTree<Integer>)rbtree).BlackHeight());
     }
 
-    static void Demo(List<Integer> list, bool visibleElements) {
+    /*static void Demo(List<Integer> list, boolean visibleElements) {
         System.out.println("Clear trees...");
-        tree.Clear();
-        rbtree.Clear();
+        tree.clear();
+        //rbtree.clear();
 
         System.out.println("Try to add " + list.size() + " element(s): ");
         if(visibleElements) {
@@ -169,45 +170,45 @@ public class Program {
         }
 
         int addedInTree = Fill(list, tree);
-        int addedInRBTree = Fill(list, rbtree);
+        //int addedInRBTree = Fill(list, rbtree);
         System.out.println(addedInTree + " node(s) was added in binary search tree");
         System.out.println(addedInRBTree + " node(s) was added  in red black tree");
-        System.out.println("Deleting the same " +list.size() + " eletemt(-s)...");
+        System.out.println("Deleting the same " + list.size() + " eletemt(-s)...");
         int noDelete = 0;
         int noDeleteRB = 0;
 
         for (Integer i : list) {
-            if (!tree.Delete(i)) {
+            if (!tree.delete(i)) {
                 ++noDelete;
             }
 
             if (!rbtree.Delete(i)) {
-                ++noDeleteRB;
+                //++noDeleteRB;
             }
         }
 
         System.out.println(noDelete + " value(-s) could not be deleted in tree");
-        System.out.println(noDeleteRB + " value(-s) could not be deleted in rb-tree");
+        //System.out.println(noDeleteRB + " value(-s) could not be deleted in rb-tree");
 
         System.out.println("Explanation: list.size() == addedInTree + noDelete, because list can contain duplicates");
         System.out.println("tree:    " + list.size() + " == " + addedInTree + " + " + noDelete + "   is "
                 + (list.size() == addedInTree + noDelete));
-        System.out.println("rb-tree: " + list.size() + " == " + addedInRBTree + " + " + noDeleteRB + "   is "
-                + (list.size() == addedInRBTree + noDeleteRB));
+       // System.out.println("rb-tree: " + list.size() + " == " + addedInRBTree + " + " + noDeleteRB + "   is "
+                //+ (list.size() == addedInRBTree + noDeleteRB));
 
         System.out.println("\nPrint tree:");
-        tree.Print();
-        System.out.println("\nPrint rb-tree:");
-        rbtree.Print();
+        tree.print();
+       // System.out.println("\nPrint rb-tree:");
+      //  rbtree.Print();
 
-        System.out.println($"\ntree size    = {tree.Size()}");
-        System.out.println($"rb-tree size = {rbtree.Size()}");
+        System.out.println("\ntree size    = {tree.Size()}");
+       // System.out.println($"rb-tree size = {rbtree.Size()}");
         System.out.println("\n\nPress any key to continue...");
     }
 
     static void DemoFirst() {
         System.out.println("           -= DEMO case#1 =-");
-        List<Integer> list = new List<Integer>(
+        List<Integer> list = new ArrayList<Integer>(
                 new int[] { 206043, 423728, 390348, 175224, 588825, 477871, 567496, 905545, 262916, 247149 });
         Demo(list, true);
     }
@@ -221,19 +222,19 @@ public class Program {
 
     static List<Integer> generateList(int size, int maxValue) {
         Random random = new Random();
-        List<Integer> list = new List<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
         System.out.println($"Generate {size} element(-s) in range=[1,{maxValue}]...");
 
         for (int i = 1; i <= size; i++) {
-            list.add(random.Next(1, maxValue));
+            list.add(random.next(1, maxValue));
         }
         return list;
     }
 
-    static int Fill<T>(List<T> list, ITree<T> tree) where T : IComparable<T> {
+    static int fill<T>(List<T> list, Tree<T> tree) where T : Comparable<T> {
         int count = 0;
         for(T element : list) {
-            if (tree.Add(element)) {
+            if (tree.add(element)) {
                 count++;
             }
         }
